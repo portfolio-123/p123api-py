c28f2450c40de056c51dc4ebc543e485ba5dc9e1
valmarv <marius@portfolio123.com>
2026-06-11 19:56:47 +0300

    Data series info (#4)
    * New endpoint to retrieve data series id, name, description
    
    * Review
    
    ---------
    
    Co-authored-by: Aaron <aaron@portfolio123.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="p123api",
-    version="2.3.0",
+    version="2.4.0",
     author="Portfolio123",
     author_email="info@portfolio123.com",
     description="Portfolio123 API wrapper",
@@ -13,11 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/portfolio-123/p123api-py",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License", "Operating System :: OS Independent"],
     python_requires=">=3.6",
     install_requires=["pandas", "requests"],
 )
